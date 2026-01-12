e98451c13364538d1fd6f4570407502bc9b539eb
Simpronic <marcdifiandra98@gmail.com>
2026-01-12 15:29:18 +0100

    Aggiunto pagina principale

---
+++ js/mainpage.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const plusButton = document.getElementById('plusButton');
+  const toolbarMenu = document.getElementById('toolbarMenu');
+
+  plusButton.addEventListener('click', () => {
+    const isVisible = toolbarMenu.style.display === 'flex';
+    toolbarMenu.style.display = isVisible ? 'none' : 'flex';
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!plusButton.contains(e.target) && !toolbarMenu.contains(e.target)) {
+      toolbarMenu.style.display = 'none';
+    }
+  });
+});
